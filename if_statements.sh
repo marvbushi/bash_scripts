1665e6c82613eb854d000d725ef7bacd30a3d0f1
marvbushi <m_rodriguez0@hotmail.com>
2022-03-20 12:46:44 -0400

    new file:   comments_and_variables.sh 	new file:   hello.sh 	new file:   if_statements.sh 	new file:   pass_arg_to_a_bash_script.sh 	new file:   read_user_input.sh

---
+++ if_statements.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+count=10
+
+if (( $count <= 10 )) 
+then
+ echo "condition is true"
+fi
---
+++ pass_arg_to_a_bash_script.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+echo $1 $2 $3 ' > echo $1 $2 $3'
+
+args=("$@")
+echo ${args[0]} ${args[1]} ${args[2]} ${args[3]}
+
+echo " "
+echo $@
+
+# print the number of arguments
+echo $#
